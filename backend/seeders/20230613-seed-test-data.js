75da9ad888a81e1654f1ae754bb6c6670f9274f0
JordanBandur <jordanbandur@hotmail.ca>
2024-06-14 15:58:45 -0700

    Update seed data

--- backend/seeders/20230613-seed-test-data.js
+++ backend/seeders/20230613-seed-test-data.js
@@ -28,12 +28,37 @@ module.exports = {
     // Insert OBD data
     await queryInterface.bulkInsert('obds', [{
       data: JSON.stringify({
+        rpm: 3000,
         speed: 60,
-        engineLoad: 75,
-        coolantTemperature: 90
+        fuelLevel: 50,
+        throttlePosition: 30,
+        intakeAirTemperature: 20,
+        coolantTemperature: 90,
+        batteryVoltage: 14,
+        dtcs: 2,
+        engineLoad: 40,
+        fuelPressure: 50,
+        shortTermFuelTrim: 5,
+        longTermFuelTrim: 8,
+        massAirFlowRate: 30,
+        o2SensorVoltage: 0.8,
+        timingAdvance: 15,
+        manifoldAbsolutePressure: 100,
+        absoluteThrottlePosition: 20,
+        controlModuleVoltage: 3.5,
+        fuelRailPressure: 200,
+        egrCommanded: 50,
+        egrError: 10,
+        evaporativePurge: 30,
+        warmupsSinceDtcCleared: 10,
+        distanceTraveledSinceDtcCleared: 500,
+        ambientAirTemperature: 25,
+        engineOilTemperature: 100,
+        fuelInjectionTiming: 10,
+        engineFuelRate: 2
       }),
       vehicle_id: vehicle[0].id,
-      created_at: new Date(),
+      created_at: new Date()
     }]);
   },
 
