f9d05c54058cce7c5f4903fa84eb95e6d923c38a
JordanBandur <jordanbandur@hotmail.ca>
2024-06-16 21:43:13 -0700

    Comment out code that isn't running

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const emailRoutes = require('./src/emailRoutes');
-const textRoutes = require('./src/textRoutes'); 
 
 const app = express();
 
@@ -12,21 +10,23 @@ app.use(bodyParser.json());
 
 // Routes
 //const authRoutes = require('./routes/authRoutes');
-//const vehicleRoutes = require('./routes/vehicleRoutes');
+const vehicleRoutes = require('./routes/vehicleRoutes');
 //const maintenanceRoutes = require('./routes/maintenanceRoutes');
 //const notificationRoutes = require('./routes/notificationRoutes');
-//const obdRoutes = require('./routes/obdRoutes');
+const obdRoutes = require('./routes/obdRoutes');
 //const serviceReminderRoutes = require('./routes/serviceReminderRoutes');
-const dtcRoutes = require('./routes/dtcRoutes');
+//const dtcRoutes = require('./routes/dtcRoutes');
+//const emailRoutes = require('./src/emailRoutes');
+//const textRoutes = require('./src/textRoutes'); 
 
 //app.use('/api/auth', authRoutes);
-//app.use('/api/vehicles', vehicleRoutes);
+app.use('/api/vehicles', vehicleRoutes);
 //app.use('/api/maintenance', maintenanceRoutes);
 //app.use('/api/notifications', notificationRoutes);
-//app.use('/api/obd', obdRoutes);
+app.use('/api/obd', obdRoutes);
 //app.use('/api/service-reminders', serviceReminderRoutes);
-app.use('/api/email', emailRoutes);
-app.use('/api/text', textRoutes);
-app.use('/api/dtc', dtcRoutes);
+//app.use('/api/email', emailRoutes);
+//app.use('/api/text', textRoutes);
+//app.use('/api/dtc', dtcRoutes);
 
 module.exports = app;
