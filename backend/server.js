2876299d6a0cad2b24a41a76987b6053a2544dd0
JordanBandur <jordanbandur@hotmail.ca>
2024-06-16 18:33:51 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,18 @@
-
+const express = require('express');
+const path = require('path');
 const app = require('./app');
 const dotenv = require('dotenv');
 const { sequelize } = require('./models');
-const express = require('express');
-const DTC = require('./models/DTC');
 
 dotenv.config();
 
-console.log('Port from .env:', process.env.PORT);
-
 const PORT = process.env.PORT || 5000;
 
-app.get('/dtcs', async (req, res) => {
-  try {
-    const dtcs = await DTC.findAll();
-    res.json(dtcs);
-  } catch (error) {
-    console.error('Error fetching DTCs:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+app.use((req, res) => {
+  res.status(404).send('Page not found');
 });
 
+// Sync database and start server
 sequelize.sync().then(() => {
   console.log('Connected to PostgreSQL');
   app.listen(PORT, () => {
