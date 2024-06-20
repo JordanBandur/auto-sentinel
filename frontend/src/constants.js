06f3e10ee954ff0c0f58d708a4554885befa116c
JordanBandur <jordanbandur@hotmail.ca>
2024-06-19 17:45:37 -0700

    Refactor constants to be in seperate file

---
+++ frontend/src/constants.js
@@ -0,0 +1,35 @@
+export const metricDescriptions = {
+  rpm: "Revolutions per minute (RPM) is the number of times the engine's crankshaft completes one full rotation per minute.",
+  speed: "The current speed of the vehicle in miles per hour (mph).",
+  fuelLevel: "The percentage of fuel remaining in the fuel tank.",
+  throttlePosition: "The position of the throttle as a percentage.",
+  intakeAirTemperature: "The temperature of the air entering the engine.",
+  coolantTemp: "The temperature of the engine coolant.",
+  batteryVoltage: "The voltage of the vehicle's battery.",
+  engineLoad: "Represents the percentage of the engine's capacity being used.",
+  fuelPressure: "Indicates the pressure of the fuel in the fuel rail, important for proper fuel injection.",
+  shortTermFuelTrim: "Adjustments made by the engine control unit to the fuel mixture for optimal combustion.",
+  longTermFuelTrim: "Long-term adjustments to the fuel mixture to correct any persistent deviations from the ideal mixture.",
+  massAirFlowRate: "The amount of air entering the engine, critical for fuel management.",
+  o2SensorVoltage: "Voltage output from the oxygen sensor, which helps in adjusting the air-fuel ratio.",
+  timingAdvance: "Indicates how much the spark timing is advanced from the top dead center.",
+  manifoldAbsolutePressure: "Measures the pressure within the intake manifold, important for engine load calculations.",
+  absoluteThrottlePosition: "The actual position of the throttle plate in the throttle body.",
+  controlModuleVoltage: "Voltage supplied to the vehicle's control modules, which should be consistent for proper function.",
+  fuelRailPressure: "Pressure within the fuel rail, crucial for fuel injection accuracy.",
+  egrCommanded: "The extent to which the Exhaust Gas Recirculation (EGR) valve is open.",
+  egrError: "The difference between the commanded and actual EGR positions.",
+  evaporativePurge: "Measures the purging of fuel vapors from the fuel tank into the intake manifold.",
+  warmupsSinceDtcCleared: "The number of times the engine has been warmed up since the Diagnostic Trouble Codes were cleared.",
+  distanceSinceDtcCleared: "The distance traveled by the vehicle since the Diagnostic Trouble Codes were cleared.",
+  ambientAirTemperature: "The temperature of the air outside the vehicle.",
+  engineOilTemperature: "The temperature of the engine oil, important for engine protection and performance.",
+  fuelInjectionTiming: "The timing of the fuel injection in relation to the engine's crankshaft position.",
+  engineFuelRate: "The rate at which fuel is being consumed by the engine."
+};
+
+export const verifiedEmails = [
+  'miguelmasche@gmail.com',
+  'rodriguezruizsergio@gmail.com',
+  'jordanbandur@hotmail.ca'
+];
